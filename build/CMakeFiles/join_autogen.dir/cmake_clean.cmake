315d6c336b9fd39b9a54a4759d8b4cd93053951f
Jan Dorazil <deu439@gmail.com>
2022-09-13 23:01:03 +0200

    Stop tracking build directories.

--- build/CMakeFiles/join_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/join_autogen"
-  "join_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/join_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/wenceslas_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/wenceslas_autogen"
-  "wenceslas_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/wenceslas_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
